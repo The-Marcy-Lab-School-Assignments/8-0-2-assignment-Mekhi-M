32dd9c00b1290d4934e3b6fa54e7759f90274b50
Mekhi-M <mekhim0731@gmail.com>
2024-09-05 14:30:10 -0400

    middleman created

--- giphy-search/src/App.jsx
+++ giphy-search/src/App.jsx
@@ -1,34 +1,38 @@
-import NavBar from './components/NavBar'
-import GifContainer from './components/GifContainer'
-import GifSearch from './components/GifSearch'
-import { handleFetch } from './utils';
-import { useState, useEffect } from 'react';
-import { API_KEY } from './config';
+/** @format */
 
-const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`
+import NavBar from "./components/NavBar";
+import GifContainer from "./components/GifContainer";
+import GifSearch from "./components/GifSearch";
+import { handleFetch } from "./utils";
+import { useState, useEffect } from "react";
+
+const URL = `/api/gifs`;
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+	const [gifs, setGifs] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    const doFetch = async () => {
-      const [data, error] = await handleFetch(URL);
-      if (error) setErrorMessage(error.message);
-      if (data) setGifs(data.data);
-    }
-    doFetch();
-  }, []);
+	useEffect(() => {
+		const doFetch = async () => {
+			const [data, error] = await handleFetch(URL);
+			if (error) setErrorMessage(error.message);
+			if (data) setGifs(data.data);
+		};
+		doFetch();
+	}, []);
 
-  return (
-    <div>
-      <NavBar color='black' title="Giphy Search" />
-      <div className="ui container">
-        <GifContainer gifs={gifs} />
-      </div>
-      <p style={{ color: "red" }}>{errorMessage}</p>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar
+				color='black'
+				title='Giphy Search'
+			/>
+			<div className='ui container'>
+				<GifContainer gifs={gifs} />
+			</div>
+			<p style={{ color: "red" }}>{errorMessage}</p>
+		</div>
+	);
 }
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,32 @@
+/** @format */
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const pathToDistFolder = path.join(__dirname, "../giphy-search/dist");
+
+const fetchData = require("./utils/fetchData");
+
+const serveStatic = express.static(pathToDistFolder);
+
+const serveGifs = async (req, res, next) => {
+	const API_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=3&rating=g`;
+	const [data, error] = await fetchData(API_URL);
+	if (error) {
+		console.log(error.message);
+		return res.status(404).send(error);
+	}
+	res.send(data);
+};
+
+app.use(serveStatic);
+
+app.get("/api/gifs", serveGifs);
+
+const port = 8080;
+app.listen(port, () => {
+	console.log(`Server is now running on http://localhost:${port}`);
+});
